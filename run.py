0872490696a208a79533436013ec62b0a6d2b266
JingBh <jingbohao@yeah.net>
2020-12-17 21:29:30 +0800

    Add argument parser.

--- run.py
+++ run.py
@@ -1,9 +1,29 @@
 #!/usr/bin/env python3
 
-# noinspection PyUnresolvedReferences
+from argparse import ArgumentParser
+
 import logging_config
 
-import studentdb_bot
+parser = ArgumentParser()
+
+parser.add_argument('-u', '--update', action='store_true',
+                    help='update the data and exit')
+
+parser.add_argument('-w', '--webhook', action='store_true',
+                    help='run in webhook mode')
+
+parser.add_argument('--debug', action='store_true',
+                    help='enable debug mode')
+
+args = parser.parse_args()
+
+if args.debug:
+    logging_config.force_debug()
 
 if __name__ == '__main__':
-    studentdb_bot.run()
+    import studentdb_bot
+
+    if args.update:
+        studentdb_bot.update_database()
+    else:
+        studentdb_bot.run()
